7a106f4e341dabea2ac5c47c213827cbb1fa2397
WINEZERO <kissloo@foxmail.com>
2023-07-26 20:28:51 +0800

    无用更新

--- libs/lib_attribdict/config.py
+++ libs/lib_attribdict/config.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
+# encoding: utf-8
 
 from libs.lib_attribdict.attribdict import AttribDict
 
-# module config_dict
+# module config
 CONFIG = AttribDict()
 
 if __name__ == '__main__':
